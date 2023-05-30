13a272ab3ae81573774a4ca0e1493e7646076558
CcLoooS <carlos.cantillo@live.com>
2023-05-30 10:42:11 -0400

    Added Button functionality and CSS Styling

--- script.js
+++ script.js
@@ -3,25 +3,16 @@
 // 16 X 16 Grid to start, than resized by either a prompt, or a selector.
 // When square is clicked, it will trigger a function in javascript. we will use DOM manipulation methods.
 
-const cdiv = document.querySelector('.box');
-
 // Add 16 divs
 for (let i = 1; i < 257; i++) {
+  const box = document.querySelector('.box');
   const div = document.createElement('div');
-  div.addEventListener("click", changeColor);
   div.addEventListener("mouseover", changeColor);
-  cdiv.appendChild(div);
-
+  box.appendChild(div);
 }
-
 let isMouseDown;
 document.addEventListener('mousedown', () => isMouseDown = true);
 document.addEventListener('mouseup', () => isMouseDown = false);
-
-function changeColor(event) {
-    if(!isMouseDown) 
-    return; this.style.backgroundColor = randomColor();
-}
 
 function randomColor() {
 
@@ -31,6 +22,36 @@ const randomBlue = Math.floor(Math.random() * 255);
 //create the string that is the ‘random color’
 const randomColor = "rgb("+randomRed+","+randomGreen+","+randomBlue+")";
 
-return randomColor;
+  return randomColor;
 }
 
+function changeColor() {
+  if (!isMouseDown) 
+    return;
+
+  if (setting === "rgb") {
+    this.style.backgroundColor = randomColor();
+    return;
+  } else if (setting === "regular") {
+    this.style.backgroundColor = "DimGray";
+    return;
+  } else if (setting === "eraser") {
+    this.style.backgroundColor = "white";
+    return;
+  }
+}
+
+const regularMode = document.getElementById('regular')
+regularMode.addEventListener("click", () => {
+  setting = "regular";
+});
+
+const rgbMode = document.getElementById('rgb')
+rgbMode.addEventListener("click", () => {
+  setting = "rgb";
+});
+
+const eraserMode = document.getElementById('eraser')
+eraserMode.addEventListener("click", () => {
+  setting = "eraser";
+});
